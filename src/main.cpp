289eb3c6f838b620b156ab87f5f24281ac4777dd
Thomas Schärer <muengger@hotmail.com>
2022-10-18 18:26:59 +0200

    und jetzt erst recht nachli schnöller

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,7 @@ void setup() {
 void loop() {
   static unsigned long oldmilisecond100Hz = 0;
   static unsigned long oldmilisecond10Hz = 0;
-  static unsigned long oldmilisecond5Hz = 0;
+  static unsigned long oldmilisecond20Hz = 0;
   static unsigned long oldmilisecond1Hz = 0;
   unsigned long newmillisecond = millis();
   if((oldmilisecond100Hz+10) < newmillisecond){ //100Hz
@@ -49,19 +49,20 @@ void loop() {
     cOdrive.CyclicUpdate();
 
   } 
-
+  if((oldmilisecond20Hz+50) < newmillisecond){ //20Hz
+    oldmilisecond20Hz = newmillisecond;
+    cTrottle.Update();
+    cStateMaschine.CyclicRun(); 
+    
+  }
   if((oldmilisecond10Hz+100) < newmillisecond){ //10Hz
     oldmilisecond10Hz = newmillisecond;
     
-    cTrottle.Update();
-    cStateMaschine.CyclicRun();
+
+    cDisplay.ComputeCyclic();
   } 
 
-  if((oldmilisecond5Hz+200) < newmillisecond){ //5Hz
-    oldmilisecond5Hz = newmillisecond;
-    
-    cDisplay.ComputeCyclic();
-  }
+
 
   if((oldmilisecond1Hz+1000) < newmillisecond){ //1Hz
     oldmilisecond1Hz = newmillisecond;
